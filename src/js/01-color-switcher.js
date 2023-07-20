5fe0c1c5ff5cee8e879fe5f4b3198ea58ea5daf5
yuliatos12 <125970340+yuliatos12@users.noreply.github.com>
2023-07-20 19:43:40 +0300

    +

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startEl = document.querySelector('[data-start]');
+const stopEl = document.querySelector('[data-stop]');
 
+startEl.addEventListener('click', onStartClick);
+stopEl.addEventListener('click', onStopClick);
+
+let timerId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+function onStartClick () {
+   timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+    startEl.disabled = true;
+    stopEl.disabled = false;
+}
+ function onStopClick () {
+clearInterval(timerId);
+startEl.disabled = false;
+stopEl.disabled = true;
+ }
